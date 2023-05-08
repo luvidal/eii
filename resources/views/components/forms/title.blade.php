08490d89a4c892742f074dd6e7dfd7181f91514b
Luis Vidal <luvidal@edictus.com>
2023-05-08 15:00:46 -0400

    light/dark improvements

--- resources/views/components/forms/title.blade.php
+++ resources/views/components/forms/title.blade.php
@@ -4,7 +4,7 @@
     $props = "$style";
 
 @endphp
-<div {!! $props !!}  class='text-muted p-1 {{ $attributes->get("class") }}'>
+<div {!! $props !!}  class='text p-1 {{ $attributes->get("class") }}'>
     @if(isset($icon))<i class='fa {{ $icon }} me-1'></i>@endif
     @if(isset($text))<span class='text-uppercase'>{{ $text }}</span>@endif
 </div>
